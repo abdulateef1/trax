14959dae5139fe073158caa4d5ccc15191793993
abdulateef1 <odunbakuabdulateef@gmail.com>
2022-08-29 22:58:17 +0000

    trax

---
+++ main.js
@@ -0,0 +1,30 @@
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show');//javscript  css to display nav menu
+        });
+    }
+};
+showMenu('nav-toggle', 'nav-menu');
+
+//Activating and removing menu bar on mobile screen
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+//fixed header
+function scrollHeader() {
+    if(window.scrollY > 100) {
+        document.querySelector('.header').classList.add('fixed');
+    } else {
+        document.querySelector('.header').classList.remove('fixed');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
